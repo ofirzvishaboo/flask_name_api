a6fd1343546f05408c9122886b23581cf2a633c2
ofirzvishaboo <ofirfirst@gmail.com>
2024-02-17 19:44:08 +0200

    changing dockerfile

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-until nc -z db 3306; do
+until curl -s http://db:3306 &> /dev/null; do
   >&2 echo "Waiting for MySQL to start..."
   sleep 1
 done
 
-exec docker-entrypoint.sh python app.py
+exec docker-entrypoint.sh devops-rest
